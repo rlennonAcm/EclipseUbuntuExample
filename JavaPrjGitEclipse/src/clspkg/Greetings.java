4c22d989811161ceb5a9f10bd370090cde956eb2
rlennonAcm <rlennon@acm.org>
2018-05-25 19:39:55 +0100

    An extra greeting is printed to screen.

--- JavaPrjGitEclipse/src/clspkg/Greetings.java
+++ JavaPrjGitEclipse/src/clspkg/Greetings.java
@@ -8,7 +8,7 @@ public class Greetings {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Greetings LYIT Student!");
-
+		System.out.println("Greetings Everyone!");
 	}
 
 }
